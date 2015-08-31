e1fae2040f64d610a99a98083b6eca2ea89c83cb
Cameron Gray <cameron@camerongray.me>
2015-08-31 18:23:52 +0100

    Removed class to simplify code and remove state

--- configfile-helper/configfile-helper.py
+++ configfile-helper/configfile-helper.py
@@ -5,63 +5,61 @@ import sys
 
 CONFIG_FILE_PATH = os.path.join(os.path.dirname(__file__), "config.ini")
 
-class ConfigFileHelper():
-    def main(self):
-        parser = argparse.ArgumentParser(
-            description="Configuration File Helper")
+def main():
+    parser = argparse.ArgumentParser(
+        description="Configuration File Helper")
 
-        action_group = parser.add_mutually_exclusive_group()
+    action_group = parser.add_mutually_exclusive_group()
 
-        action_group.add_argument("--set-repo",
-            action="store", metavar="PATH",
-            help="Set the path to the config file repository to be used")
+    action_group.add_argument("--set-repo",
+        action="store", metavar="PATH",
+        help="Set the path to the config file repository to be used")
 
-        action_group.add_argument("--set-var-file",
-            action="store", metavar="PATH",
-            help="Set the path to the variable file")
+    action_group.add_argument("--set-var-file",
+        action="store", metavar="PATH",
+        help="Set the path to the variable file")
 
-        args = parser.parse_args()
+    args = parser.parse_args()
 
-        self.config = configparser.ConfigParser()
-        self.config.read(CONFIG_FILE_PATH)
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE_PATH)
 
-        # Check which method was specified and route it to the
-        # appropriate method
-        if args.set_repo:
-            self._set_repo(args.set_repo)
-        elif args.set_var_file:
-            self._set_var_file(args.set_var_file)
-        else:
-            parser.print_help()
+    # Check which method was specified and route it to the
+    # appropriate method
+    if args.set_repo:
+        set_repo(config, args.set_repo)
+    elif args.set_var_file:
+        set_var_file(config, args.set_var_file)
+    else:
+        parser.print_help()
 
-    def _set_repo(self, repo_path):
-        if not os.path.isdir(repo_path):
-            print("Path specified is not a valid directory")
-            sys.exit()
+def set_repo(config, repo_path):
+    if not os.path.isdir(repo_path):
+        print("Path specified is not a valid directory")
+        sys.exit()
 
-        self._set_config_value("Paths", "repo_directory", repo_path)
-        self._save_config_file()
-        print("Repo path has been updated successfully")
+    set_config_value(config, "Paths", "repo_directory", repo_path)
+    save_config_file(config)
+    print("Repo path has been updated successfully")
 
-    def _set_var_file(self, file_path):
-        if not os.path.isfile(file_path):
-            print("Path specified is not a file")
-            sys.exit()
+def set_var_file(config, file_path):
+    if not os.path.isfile(file_path):
+        print("Path specified is not a file")
+        sys.exit()
 
-        self._set_config_value("Paths", "variable_file", file_path)
-        self._save_config_file()
-        print("Variable file path has been updated successfully")
+    set_config_value(config, "Paths", "variable_file", file_path)
+    save_config_file(config)
+    print("Variable file path has been updated successfully")
 
-    def _save_config_file(self):
-        with open(CONFIG_FILE_PATH, "w") as configfile:
-            self.config.write(configfile)
+def save_config_file(config):
+    with open(CONFIG_FILE_PATH, "w") as configfile:
+        config.write(configfile)
 
-    def _set_config_value(self, section, index, value):
-        if section not in self.config:
-            self.config[section] = {}
+def set_config_value(config, section, index, value):
+    if section not in config:
+        config[section] = {}
 
-        self.config[section][index] = value
+    config[section][index] = value
 
 if __name__ == "__main__":
-    c = ConfigFileHelper()
-    c.main()
+    main()
